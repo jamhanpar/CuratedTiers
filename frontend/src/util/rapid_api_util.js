6ce7f83199d5bc9b4b9fce92b7a8f6c1858d1083
David Suh <davidisuh93@gmail.com>
2021-02-03 18:25:02 -0500

    change api key

--- frontend/src/util/rapid_api_util.js
+++ frontend/src/util/rapid_api_util.js
@@ -1,12 +1,12 @@
 import Keys from '../../config/keys';
-var axios = require("axios").default;
+const axios = require("axios").default;
 
-var options = {
+const options = {
   method: 'GET',
   url: 'https://amazon-product-reviews-keywords.p.rapidapi.com/product/search',
   params: {keyword: 'placeholder', page: '1', country: 'US', category: 'aps'},
   headers: {
-    'x-rapidapi-key': `${Keys.rapidAPIKey}`,
+    'x-rapidapi-key': Keys.rapidAPIKey,
     'x-rapidapi-host': 'amazon-product-reviews-keywords.p.rapidapi.com'
   }
 };
