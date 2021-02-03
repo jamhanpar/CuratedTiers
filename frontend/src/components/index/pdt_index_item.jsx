5ca708fd652298d38792053eb2549d9769ac802c
philipk8 <philipk876@gmail.com>
2021-02-03 12:37:26 -0600

    updated with liked container

--- frontend/src/components/index/pdt_index_item.jsx
+++ frontend/src/components/index/pdt_index_item.jsx
@@ -1,12 +1,23 @@
 import React from "react";
+import LikedContainer from '../like/like_container'
+
 
 const PdtIndexItem = (props) => {
+  // newObj = {}
+  // props.map ( pdt => newObj[key] = pdt[0])
+
   return (
       <li>
         Title: {props.pdt[9]} <br />
         Price: {props.pdt[1]} <br />
         Score: {props.pdt[2]} <br />
         <img src={props.pdt[8]} alt="" />
+        <LikedContainer
+          // props={props}
+          // props={newObj}
+          // asin={newObj.asin}
+          // newObj.asin
+        />
       </li>
     )
 }
