3eb88addeb24512a1d8da86dff25b0670cddfc54
David Suh <davidisuh93@gmail.com>
2021-02-03 16:48:08 -0500

    api key fix

--- frontend/src/util/rapid_api_util.js
+++ frontend/src/util/rapid_api_util.js
@@ -6,7 +6,7 @@ var options = {
   url: 'https://amazon-product-reviews-keywords.p.rapidapi.com/product/search',
   params: {keyword: 'placeholder', page: '1', country: 'US', category: 'aps'},
   headers: {
-    'x-rapidapi-key': Keys.rapidAPIKey,
+    'x-rapidapi-key': process.env.RAPID_API_KEY,
     'x-rapidapi-host': 'amazon-product-reviews-keywords.p.rapidapi.com'
   }
 };
