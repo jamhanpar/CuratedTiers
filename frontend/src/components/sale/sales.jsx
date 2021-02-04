1d3b92bebcd254851894942812eb7178b04ba8cb
James <james.hansung.park@gmail.com>
2021-02-03 19:52:01 -0500

    updated sales container

--- frontend/src/components/sale/sales.jsx
+++ frontend/src/components/sale/sales.jsx
@@ -1,34 +1,62 @@
 import React from "react";
+import ShoppingLoadIcon from "../../img/Shopping-1.5s-200px.gif";
 import '../stylesheets/content.css';
-import '../stylesheets/reset.css';
 
 class SalesPage extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentWillUnmount() {
+    this.props.resetProducts();
+  }
+
+  componentDidMount() {
+    const searchTerm = localStorage.searchTerm;
+
+    this.props.fetchProducts(searchTerm);
+  }
+
   render() {
-    return (
-      <section className="content-container">
-        <div className="homepage-filter">
-            <ul className="filter-options-list">
-                <li>Trending</li>
-                <li>New</li>
-                <li>Most popular</li>
-            </ul>
+    if (this.props.products.length === undefined) {
+      return (
+        <div className="loading-gif">
+          <img src={ShoppingLoadIcon} alt="Loading..." className="shopping-gif"/>
         </div>
-        <div className="product-list-container">
-          <div className="product-card">
+      );
+    } else {
+      return (
+        <section className="content-container">
+          <div className="homepage-filter">
+            <ul className="filter-options-list">
+              <li>Trending</li>
+              <li>New</li>
+              <li>Most popular</li>
+            </ul>
+          </div>
+          <div className="product-list-container">
+            <div className="product-card">
               <div className="product-card-price-container">
-                  <button className="product-card-price">$300</button>
+                <button className="product-card-price">$300</button>
               </div>
               <div className="product-card-img-container">
-                  <img className="product-card-img" src="https://res.cloudinary.com/hcu8jcnmr/image/upload/c_fit,w_600,h_600/swedradzazzc0ilphx2l.jpg" alt="product-img"/>
+                <img
+                  className="product-card-img"
+                  src="https://res.cloudinary.com/hcu8jcnmr/image/upload/c_fit,w_600,h_600/swedradzazzc0ilphx2l.jpg"
+                  alt="product-img"
+                />
               </div>
               <div className="product-card-info-container">
-                  <h1 className="product-card-info">H7 Wireless Over-Ear Headphones, Natural</h1>
-                  <p className="product-card-info">by B{'&'}O PLAY</p>
+                <h1 className="product-card-info">
+                  H7 Wireless Over-Ear Headphones, Natural
+                </h1>
+                <p className="product-card-info">by B{"&"}O PLAY</p>
               </div>
+            </div>
           </div>
-        </div>
-      </section>
-    );
+        </section>
+      )
+    }
   }
 }
 
