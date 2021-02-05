172f60dc7237fb43282d26d4d189d3444f4ef828
James <james.hansung.park@gmail.com>
2021-02-05 11:59:16 -0500

    udpated css

--- frontend/src/components/show/show.jsx
+++ frontend/src/components/show/show.jsx
@@ -1,21 +1,20 @@
 import React from "react";
-import LikedContainer from '../universal/like/like_container'
-import './show'
+import LikedContainer from "../universal/like/like_container";
+import "./show";
 
 const ShowPage = (props) => {
-
   // const asin = props.pdt[0]
   // const score = props.pdt[2]
-  const beforePrice = props.pdt.pdt[3]
+  const beforePrice = props.pdt.pdt[3];
   // const savingsAmount = props.pdt.pdt[4]
-  const savingsPercent = props.pdt.pdt[5]
-  const totalReviews = props.pdt.pdt[6]
-  const url = props.pdt.pdt[10]
-  
-  const title = props.pdt.pdt[9]
-  const currentPrice = props.pdt.pdt[1]
-  const rating = props.pdt.pdt[7]
-  const thumbnail = props.pdt.pdt[8]
+  const savingsPercent = props.pdt.pdt[5];
+  const totalReviews = props.pdt.pdt[6];
+  const url = props.pdt.pdt[10];
+
+  const title = props.pdt.pdt[9];
+  const currentPrice = props.pdt.pdt[1];
+  const rating = props.pdt.pdt[7];
+  const thumbnail = props.pdt.pdt[8];
 
   // const pdt = props.pdt.pdt
 
@@ -25,10 +24,6 @@ const ShowPage = (props) => {
         <div className="show-card-img-container">
           <img className="show-card-img" src={thumbnail} alt="" />
         </div>
-        <div className="show-liked-container">
-          <LikedContainer props={props.pdt.pdtObj} />
-          {/* <p className="like-text">LIKE</p> */}
-        </div>
       </div>
 
       <div className="show-card-right">
@@ -36,9 +31,19 @@ const ShowPage = (props) => {
           <h1 className="show-card-info">{title}</h1>
           <h1 className="show-card-info-tiny">Rating: {rating}</h1>
           <h1 className="show-card-info-tiny">Reviews: {totalReviews}</h1>
-          <h1 className="show-card-info-tiny">Price: $ {Math.round(beforePrice)}</h1>
-          <h1 className="show-card-info-tiny">Sale: {Math.round(savingsPercent)} %</h1>
-          <h1 className="show-card-info-tiny">Sale Price: $ {Math.round(currentPrice)}</h1>
+          {beforePrice !== 0 ? (
+            <h1 className="show-card-info-tiny">
+              `Price: $${Math.round(beforePrice)}`
+            </h1>
+          ) : null}
+          {savingsPercent !== 0 ? (
+            <h1 className="show-card-info-tiny">
+              `Sale: ${Math.round(savingsPercent)}%`
+            </h1>
+          ) : null}
+          <h1 className="show-card-info-tiny">
+            Sale Price: ${Math.round(currentPrice)}
+          </h1>
         </div>
 
         {/* <div className="show-card-price-container">
@@ -54,6 +59,9 @@ const ShowPage = (props) => {
         </div> */}
 
         <div className="show-card-url-container">
+          <div className="show-liked-container">
+            <LikedContainer props={props.pdt.pdtObj} heartSize={"heartSize"} />
+          </div>
           <a
             className="show-card-url-btn"
             href={`${url}`}
@@ -66,7 +74,6 @@ const ShowPage = (props) => {
       </div>
     </div>
   );
-}
-
+};
 
 export default ShowPage;
